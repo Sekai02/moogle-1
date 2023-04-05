8b791ab022069638d27316fe873315b59f729156
Sekai02 <luisalejandroarteagamorales@gmail.com>
2023-04-05 17:37:17 -0400

    TFIDF class added

--- MoogleEngine/Document.cs
+++ MoogleEngine/Document.cs
@@ -6,9 +6,23 @@ public class Document
     {
         this.Title = title;
         this.Words = text.ToLower().Split(DocumentCatcher.Delims).Select(p => p.Trim()).ToArray();
+        this.WordFrequency = new Dictionary<string, int>();
+        foreach (string word in this.Words)
+        {
+            if (WordFrequency.ContainsKey(word))
+            {
+                WordFrequency[word]++;
+            }
+            else
+            {
+                WordFrequency.Add(word, 1);
+            }
+        }
     }
 
     public string Title { get; private set; }
 
     public string[] Words { get; private set; }
+
+    public Dictionary<string, int> WordFrequency { get; private set; }
 }
--- MoogleEngine/Moogle.cs
+++ MoogleEngine/Moogle.cs
@@ -3,9 +3,9 @@
 public static class Moogle
 {
     static Document[] AllDocs = DocumentCatcher.ReadDocumentsFromFolder("/media/sekai02/Datos/School/moogle-1/Content");
-
+    static double x=TFIDF.CalculateTermFrequency(AllDocs[0].WordFrequency,"project");
     public static SearchResult Query(string query)
-    {  
+    {
         SearchItem[] items = new SearchItem[3] {
             new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.9f),
             new SearchItem("Hello World", "Lorem ipsum dolor sit amet", 0.5f),
